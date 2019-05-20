419d0b91badaadb77ef92a818aa62ab3fa51a0ee
rlllzk <rezkysy@gmail.com>
2019-05-20 20:29:44 +0700

    rapikan url

--- pos/urls.py
+++ pos/urls.py
@@ -6,5 +6,15 @@ from . import views
 urlpatterns = [
     path('pos/', views.billing, name='billing'),
     path('billing/order', views.order, name='order'),
+    path('ooder', views.ooder),
+    path('show',views.show, name='show'),
+    path('edit/<int:id>', views.edit),  
+    path('update/<int:id>', views.update, name='update'),  
+    path('delete/<int:id>', views.destroy),
+    path('pooder', views.pooder, name='pooder'),
+    path('pshow',views.pshow, name='pshow'),
+    path('pedit/<int:id>', views.pedit),  
+    path('pupdate/<int:id>', views.pupdate, name='pupdate'),  
+    path('pdelete/<int:id>', views.pdestroy),
 
 ]
--- restocash/urls.py
+++ restocash/urls.py
@@ -27,16 +27,7 @@ router.register('', views.ProductViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pos.urls')),
-    path('ooder', views.ooder),
-    path('show',views.show, name='show'),
-    path('edit/<int:id>', views.edit),  
-    path('update/<int:id>', views.update, name='update'),  
-    path('delete/<int:id>', views.destroy),
-    path('pooder', views.pooder, name='pooder'),
-    path('pshow',views.pshow, name='pshow'),
-    path('pedit/<int:id>', views.pedit),  
-    path('pupdate/<int:id>', views.pupdate, name='pupdate'),  
-    path('pdelete/<int:id>', views.pdestroy),
+    
     path('accounts/profile/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')), 
     path('', include('django.contrib.auth.urls')), 
